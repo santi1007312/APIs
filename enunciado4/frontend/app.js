dbe5173b0198b5a648ace80fb1214884ad02e451
gratos356 <andressergio1210@gmail.com>
2026-04-19 18:10:21 -0500

    feat realisacion del ejercicio 1 de la guia de apis

---
+++ enunciado4/frontend/app.js
@@ -0,0 +1,4 @@
+import {createComments,deleteComments,getComments} from "./modules/coments/index.js";
+import {createPost,getPost,deletePost} from "./modules/posts/index.js";
+import {getUsers,getUsersByid,createUsers,deleteUsers} from "./modules/users/index.js";
+
