8f9773d01e6349913bee37896e0c46cd673842ea
gratos356 <andressergio1210@gmail.com>
2026-04-19 21:28:12 -0500

    feat realisacion del ejercicio 4 eliminacion de post segura

--- enunciado4/frontend/app.js
+++ enunciado4/frontend/app.js
@@ -1,4 +1,23 @@
-import {createComments,deleteComments,getComments} from "./modules/coments/index.js";
-import {createPost,getPost,deletePost} from "./modules/posts/index.js";
-import {getUsers,getUsersByid,createUsers,deleteUsers} from "./modules/users/index.js";
+import {get,destroy} from './modules/untils/index.js';
 
+const eliminarPostSeguro = async() =>{
+    const comentarios = await get("comments");
+    console.table(comentarios);
+    alert("Se mostraron los comentarios en la consola. !!Revise si el post que desea eliminar tiene comentarios asociados.!!");
+    const postId = prompt("Ingrese el ID del post que desea eliminar:");
+
+    const tieneComentarios = comentarios.some(coment => Number(coment.postId) === Number(postId));
+
+    if (tieneComentarios) {
+        alert("No se puede eliminar el post porque tiene comentarios asociados.");
+    }else{
+        const eliminado = await destroy("posts", postId);
+        if (eliminado){
+            alert(`El post ${postId} ha sido eliminado exitosamente.`);
+        }else{
+            alert(`Error al eliminar el post ${postId}.`);
+        }
+    }
+}
+
+eliminarPostSeguro();
